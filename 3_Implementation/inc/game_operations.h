5e6c830fe4d76c40a3e28e9571abb034b5e4d85a
phani95505 <dandamudi.phani99@gmail.com>
2022-02-11 14:37:20 +0530

    Added

---
+++ 3_Implementation/inc/game_operations.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int checkwin(); 
+void board();
+
+
+char mark;
+    do
+    {
+        board();
+        player = (player % 2) ? 1 : 2;
+
+
+void board()
+{
+    system("cls");
