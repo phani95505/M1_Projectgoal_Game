ff8eaf3a24f7cb24380c4f4d5951764a62cc4623
phani95505 <dandamudi.phani99@gmail.com>
2022-03-02 19:21:59 +0530

    added

--- 3_Implementation/inc/game_operations.h
+++ 3_Implementation/inc/game_operations.h
@@ -1,17 +1,4 @@
 #include <stdio.h>
-
-
-int checkwin(); 
-void board();
-
-
-char mark;
-    do
-    {
-        board();
-        player = (player % 2) ? 1 : 2;
-
-
-void board()
-{
-    system("cls");
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
